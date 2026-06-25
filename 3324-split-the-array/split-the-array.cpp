d82afd3f9411e204c73802ca97e22d4e046512c3
Roshan Raj Gupta <roshan92793@gmail.com>
2026-06-25 14:31:11 +0530

    Time: 1 ms (48.07%) | Memory: 25.3 MB (34.41%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int x:nums){
+            freq[x]++;
+            if(freq[x]>2) return false;
+        }return true;
+    }
+};
